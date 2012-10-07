fd6e6552e5d226ca5ea8ce87a39df7d2f605da79
Bhasker Kode <bosky101@gmail.com>
2012-10-07 19:55:40 +0530

    Initial Commit

---
+++ primes/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  primes
+//
+//  Created by kode on 10/7/12.
+//  Copyright (c) 2012 Bosky. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
